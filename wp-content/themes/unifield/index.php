13eaea93fb9357e9fd4f616b2d29c38e64cb9fb8
coolJenny <coolJenny009g@gmail.com>
2018-03-19 02:09:24 -0400

    ...

--- wp-content/themes/unifield/index.php
+++ wp-content/themes/unifield/index.php
@@ -14,6 +14,298 @@ get_header();
 ?>
 <div class="container">
      <div class="page_content">
+        <div id="header-carousel" class="carousel slide carousel-fade header-carousel" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#header-carousel" data-slide-to="0" class="active"></li>
+                <li data-target="#header-carousel" data-slide-to="1"></li>   
+            </ol>   
+            <div class="carousel-inner"> 
+                <div class="item active"> 
+                    <img src="assets/img/slider-images/image1.jpg" alt="Specifie an alternate text for an image" class="img-responsive">
+                    <div class="carousel-caption header-carousel-caption">
+                        <h1 class="text-uppercase text-shadow">We are a nationally ranked fullservice remodeling and build contractor</h1>
+                        <!-- <a href="project-inner.html" class="btn white-btn">View Project</a> -->
+                    </div>
+                </div>                
+                <div class="item">        
+                    <img src="assets/img/slider-images/image2.jpg" alt="Specifie an alternate text for an image" class="img-responsive">
+                    <div class="carousel-caption header-carousel-caption">
+                        <h1 class="text-uppercase text-shadow">Beautiful way to show your work</h1>
+                        <!-- <a href="project-inner.html" class="btn white-btn">View Project</a> -->
+                    </div>
+                </div>                       
+            </div>   
+        </div>
+
+        <div class="site-wrapper">
+            <div class="container text-center">
+                <div class="general-title">
+                    <h2>Who we Are?</h2>        
+                    <div class="title-devider"></div>
+                </div>
+                <div class="row">
+                    <div class="col-md-10 col-md-offset-1">
+                        <p>Larx Design Studio, we design for the hospitality and retail sectors creating exceptional hotels, bars, restaurants and private clubs worldwide. We also work with a select number of private clients creating their dream homes. Our Studio provides a variety of services including interior architecture, interior design, furniture and lighting design.</p>        
+                    </div>          
+                </div>
+            </div>
+
+            <div class="container-fluid projects padding-top">
+                <div class="general-title">
+                    <h2 class="text-uppercase">Our Services include...</h2>        
+                    <div class="title-devider"></div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6 col-md-3">
+                        <div class="project-inner">
+                            <a href="project-inner.html">      
+                                <img src="assets/img/portfolio/image1.jpg" alt="Specifie an alternate text for an image" >                    
+                                <div class="project-caption">
+                                    <div class="project-details">
+                                        <p><i class="fa fa-plus fa-lg"></i></p>     
+                                        <h3>Penthouse Walding</h3>
+                                        <p><small>2013/08</small></p>                                               
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>  
+
+                    <div class="col-sm-6 col-md-3">
+                        <div class="project-inner">
+                            <a href="project-inner.html">    
+                                <img src="assets/img/portfolio/image2.jpg" alt="Specifie an alternate text for an image">                    
+                                <div class="project-caption">
+                                    <div class="project-details">
+                                        <p><i class="fa fa-plus fa-lg"></i></p>
+                                        <h3>Bathroom</h3>
+                                        <p><small>2013/02</small></p>                                                
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>                            
+
+                    <div class="col-sm-6 col-md-3">
+                        <div class="project-inner">
+                            <a href="project-inner.html"> 
+                                <img src="assets/img/portfolio/image3.jpg" alt="Specifie an alternate text for an image">                    
+                                <div class="project-caption">
+                                    <div class="project-details">
+                                        <p><i class="fa fa-plus fa-lg"></i></p>                        
+                                        <h3>House in Town</h3>
+                                        <p><small>2012/01</small></p>                            
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>                    
+
+                    <div class="col-sm-6 col-md-3">
+                        <div class="project-inner">
+                            <a href="project-inner.html">  
+                                <img src="assets/img/portfolio/image4.jpg" alt="Specifie an alternate text for an image">                    
+                                <div class="project-caption">
+                                    <div class="project-details">
+                                        <p><i class="fa fa-plus fa-lg"></i></p>                        
+                                        <h3>House Kitchen</h3>
+                                        <p><small>2014/02</small></p>                            
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>        
+
+                    <div class="col-sm-6 col-md-3">
+                        <div class="project-inner">
+                            <a href="project-inner.html">    
+                                <img src="assets/img/portfolio/image5.jpg" alt="Specifie an alternate text for an image">                    
+                                <div class="project-caption">
+                                    <div class="project-details">
+                                        <p><i class="fa fa-plus fa-lg"></i></p>
+                                        <h3>VILLA IN CANNES</h3>
+                                        <p><small>2013/08</small></p>                                                
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>                        
+
+                    <div class="col-sm-6 col-md-3">
+                        <div class="project-inner">
+                            <a href="project-inner.html">  
+                                <img src="assets/img/portfolio/image6.jpg" alt="Specifie an alternate text for an image">                    
+                                <div class="project-caption">
+                                    <div class="project-details">
+                                        <p><i class="fa fa-plus fa-lg"></i></p>
+                                        <h3>Penthouse in New York</h3>
+                                        <p><small>2013/08</small></p>                                                
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>                    
+
+                    <div class="col-sm-6 col-md-3">
+                        <div class="project-inner">
+                            <a href="project-inner.html">   
+                                <img src="assets/img/portfolio/image7.jpg" alt="Specifie an alternate text for an image">                    
+                                <div class="project-caption">
+                                    <div class="project-details">
+                                        <p><i class="fa fa-plus fa-lg"></i></p>
+                                        <h3>Kitchen</h3>
+                                        <p><small>2013/08</small></p>                                                    
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>                    
+
+                    <div class="col-sm-6 col-md-3">
+                        <div class="project-inner">
+                            <a href="project-inner.html">    
+                                <img src="assets/img/portfolio/image8.jpg" alt="Specifie an alternate text for an image">                        
+                                <div class="project-caption">
+                                    <div class="project-details">
+                                        <p><i class="fa fa-plus fa-lg"></i></p>
+                                        <h3>DUSSELDORF</h3>
+                                        <p><small>2013/08</small></p>                    
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="all-projects text-center text-uppercase">
+                <a href="projects.html" class="btn orange-btn">
+                    Click to receive a free estimate
+                </a>
+            </div>
+
+            <div class="container padding-top text-center">
+                <div class="general-title">
+                    <h2 class="text-uppercase">Testimonials</h2>        
+                    <div class="title-devider"></div>
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>    
+                <div class="row padding-top"> 
+                    <div class="col-sm-4 col-md-4">  
+                        <a href="services.html">                                
+                            <i class="fa fa-home fa-3x"></i>     
+                        </a> 
+                        <div class="service-title">      
+                            <h3>Interior Design</h3> 
+                        </div>   
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci.</p>
+                    </div>
+
+                    <div class="col-sm-4 col-md-4">
+                        <a href="services.html">
+                            <i class="fa fa-building fa-3x"></i>          
+                        </a> 
+                        <div class="service-title">
+                            <h3>Architecture</h3>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci.</p>
+                    </div>                        
+
+                    <div class="col-sm-4 col-md-4">
+                        <a href="services.html">
+                            <i class="fa fa-file-text-o fa-3x"></i> 
+                        </a>
+                        <div class="service-title">
+                            <h3>Design Consultations</h3>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci.</p>                 
+                    </div>  
+                </div>
+
+                <div class="padding-top padding-bottom">  
+                    <a href="services.html" class="btn black-btn">
+                        View all Services
+                    </a>
+                </div>
+            </div>
+
+            <section class="bg-color padding-top text-center">
+                <div class="container">
+                    <div class="general-title">
+                        <h2>From Blog</h2>
+                        <div class="title-devider"></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-4 col-md-4">
+                            <div class="thumbnail blog-post">
+                                <a href="blog-inner.html">
+                                    <img src="assets/img/blog/image1.jpg" alt="Specifie an alternate text for an image">
+                                </a>
+                                <div class="caption">
+                                    <div class="blog-inner-title">
+                                        <a href="blog-inner.html">
+                                            <h3>Curabitur auctor vulputate quam facilisis eu dolor</h3>
+                                        </a>
+                                    </div>
+                                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>                        
+                                    <div class="blog-meta">
+                                        <span><i class="fa fa-calendar"></i> <a href="blog-inner.html">April 01, 2014</a></span>
+                                        <span><i class="fa fa-comment"></i> <a href="blog-inner.html">03 Comments</a></span>
+                                        <span><i class="fa fa-eye"></i> 84 Views</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>                        
+
+                        <div class="col-sm-4 col-md-4">
+                            <div class="thumbnail blog-post">
+                                <a href="blog-inner.html">
+                                    <img src="assets/img/blog/image2.jpg" alt="Specifie an alternate text for an image"> 
+                                </a>
+                                <div class="caption">
+                                    <div class="blog-inner-title">
+                                        <a href="blog-inner.html">
+                                            <h3>Lorem ipsum dolor sit amet, magna adipiscing elit</h3>
+                                        </a>
+                                    </div>
+                                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>                        
+                                    <div class="blog-meta">
+                                        <span><i class="fa fa-calendar"></i> <a href="blog-inner.html">April 01, 2014</a></span>
+                                        <span><i class="fa fa-comment"></i> <a href="blog-inner.html">03 Comments</a></span>
+                                        <span><i class="fa fa-eye"></i> 84 Views</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>                        
+
+                        <div class="col-sm-4 col-md-4">
+                            <div class="thumbnail blog-post">
+                                <a href="blog-inner.html">
+                                    <img src="assets/img/blog/image3.jpg" alt="Specifie an alternate text for an image">
+                                </a>
+                                <div class="caption">
+                                    <div class="blog-inner-title">
+                                        <a href="blog-inner.html">
+                                            <h3>Duis in tortor tortor magna euismod id rutrum eros</h3>
+                                        </a>
+                                    </div>
+                                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>                    
+                                    <div class="blog-meta">
+                                        <span><i class="fa fa-calendar"></i> <a href="blog-inner.html">April 01, 2014</a></span>
+                                        <span><i class="fa fa-comment"></i> <a href="blog-inner.html">03 Comments</a></span>
+                                        <span><i class="fa fa-eye"></i> 84 Views</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>                        
+
+                    </div>
+                </div>
+
+                <div class="text-center padding-top padding-bottom">
+                    <a href="blog.html" class="btn black-btn">See All Posts</a>
+                </div>                
+            </section>
+        </div>
         
         <div class="clear"></div>
     </div><!-- site-aligner -->
